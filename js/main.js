061852a32b53ce93488766aba714a0d525a64f3d
Mihkel Sokk <mihkelsokk@gmail.com>
2011-05-20 03:07:53 +0300

    More UI

--- js/main.js
+++ js/main.js
@@ -31,4 +31,29 @@ Q.App.prototype.bindMisc = function() {
       + ',top=' + (window.screen.height - window.screen.availHeight+50));
     console.log(popWin);
   });
+  
+  $('#gsBtn').click(function() {
+    $(this).toggleClass('gs-active');
+    $(this).toggleClass('gs-disabled');
+  });
+  
+  $('#ytBtn').click(function() {
+    $(this).toggleClass('yt-active');
+    $(this).toggleClass('yt-disabled');
+  });
+  
+  $('#scBtn').click(function() {
+    $(this).toggleClass('sc-active');
+    $(this).toggleClass('sc-disabled');
+  });
+  
+  $('#treeview .new').click(function() {
+    $(this).html('+ <input id="newPlaylist" type="text" value="New Playlist" />');
+    $('#newPlaylist').select();
+    $('#newPlaylist').blur(function() {
+      $('#treeview .new').html('+ New Playlist');
+    });
+  });
+  
+
 };
