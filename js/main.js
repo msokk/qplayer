c338e34a4a0f52527dd39035121904f75ce1d796
Mihkel Sokk <mihkelsokk@gmail.com>
2011-05-28 21:59:58 +0300

    More bindings

--- js/main.js
+++ js/main.js
@@ -28,6 +28,18 @@ Q.App = function() {
   this.on('UIPlayback', function(state) {
     console.log('Playback: %s', state);
   });
+  
+  this.on('UINewPlaylist', function(elem) {
+    console.log('New playlist: %s', elem.html());
+  });
+  
+  this.on('UIViewPlaylist', function(id) {
+    console.log('Viewing playlist: %s', id);
+  });
+  
+  this.on('UIEditPlaylist', function(id) {
+    console.log('Edited playlist: %s', id);
+  });
 };
 Q.inherit(Q.App, Q.Event);
 
@@ -35,18 +47,11 @@ Q.App.prototype.initUI = function() {
   Q.UISettings(this); //Settings btn, tab
   Q.UIVolume(this); //Volume bar
   Q.UISeekbar(this); //Seek bar
+  Q.UIPlaylist(this); //Playlists
+  Q.UITracklist(this); //Playlist tracks
+  Q.UIPlayback(this); //Playback buttons, shuffle, etc.
   Q.UIGeneric(this); //Generic
   
-        
-  $('#miniBtn').click(function() {
-    var width = 250;
-    var height = 500;
-    window.popWin = window.open('popup.html', 
-      'Player', 'menubar=no,location=no,resizable=no,scrollbars=no,status=no,'
-      + 'width=' + width + ',height=' + height + ',left=' + (window.outerWidth - (width+33))
-      + ',top=' + (window.screen.height - window.screen.availHeight+55));
-    console.log(popWin);
-  });
   
   $('#gsBtn').click(function() {
     $(this).toggleClass('gs-active');
@@ -62,24 +67,5 @@ Q.App.prototype.initUI = function() {
     $(this).toggleClass('sc-active');
     $(this).toggleClass('sc-disabled');
   });
-  
-  $('#treeview .new').click(function() {
-    $(this).html('+ <input id="newPlaylist" type="text" value="New Playlist" />');
-    $('#newPlaylist').select();
-    $('#newPlaylist').blur(function() {
-      $('#treeview .new').html('+ New Playlist');
-    });
-  });
-  
-  $('#treeview li').click(function() {
-    $('#treeview li').removeClass('clicked');
-    $(this).toggleClass('clicked');
-  });
-  
-  $('#treeview li').dblclick(function() {
-    $('#treeview li').removeClass('selected');
-    $(this).removeClass('clicked');
-    $(this).toggleClass('selected');
-  });
-
+ 
 };
--- js/ui.js
+++ js/ui.js
@@ -23,6 +23,7 @@ Q.UIVolume = function(app) {
     if(perc <= 10) {
       perc = 10;
     }
+    $('#volume-icon').css('opacity', perc/100);
     that.elem.find('.done').css('width', perc + '%');
     that.app.emit('UIVolume', Math.ceil((perc*1.1) - 11));
   };
@@ -55,6 +56,7 @@ Q.UIVolume.prototype.setVolume = function(volume) {
     perc = 10;
   }
   this.elem.find('.done').css('width', perc + '%');
+  $('#volume-icon').css('opacity', perc/100);
   this.app.emit('UIVolume', volume);
 };
 
@@ -64,13 +66,12 @@ Q.UIVolume.prototype.setVolume = function(volume) {
  */
 Q.UISettings = function(app) {
   //Settings dialog
-  $('#settingsBtn').click(function() {
+  $('#settingsBtn').click(function(e) {
     $('.settingsContainer').toggleClass('show');
-    setTimeout(function() {
-      $('#container').one('click', function() {
-        $('.settingsContainer').toggleClass('show');
-      });
-    }, 1000);
+    e.stopPropagation();
+    $('#container').one('click', function() {
+      $('.settingsContainer').removeClass('show');
+    });
   });
   
   //Accent selection
@@ -142,10 +143,189 @@ Q.UISeekbar.prototype.setProgress = function(perc) {
 };
 
 /**
- * Bind generic small objects
+ * Bind playlist
  * @param {Q.App} QPlayer
  */
-Q.UIGeneric = function(app) {
+Q.UIPlaylist = function(app) {
+  //New playlist
+  $('#treeview .new').live('click', function() {
+    $(this).html('+ <input class="editBox" id="newPlaylist" type="text" value="New Playlist" />');
+    $('#newPlaylist').select();
+    
+    var newOne = function() {
+      if($('#newPlaylist').length != 0 && $('#newPlaylist').val() != 'New Playlist') {
+        $('#treeview #list').append('<li>' + $('#newPlaylist').val() + '</li>');
+        app.emit('UINewPlaylist', $('#treeview #list li:last-child'));
+      }
+      
+      $('#treeview .new').html('+ New Playlist');
+    };
+    
+    $('#newPlaylist').blur(newOne);
+    $('#newPlaylist').keypress(function(e) {
+      if(e.charCode == 13) {
+        newOne();
+      }
+    });
+  });
+  
+  //Select playlist
+  $('#treeview #list li').live('click', function() {
+    if(!$(this).hasClass('clicked')) {
+      $('#treeview li').removeClass('clicked');
+      $(this).toggleClass('clicked');
+      app.emit('UIViewPlaylist', $(this).data('id'));
+    }
+  });
+  
+  //Change name
+  $('#treeview #list li').live('dblclick', function(e) {
+    
+    if($(this).hasClass('clicked')) {
+      var oldname = $(this).html();
+      var that = this;
+      $(this).html('<input class="editBox" id="editPlaylist" type="text" value="'+oldname+'" /> X');
+      $('#editPlaylist').select();
+      var editOne = function() {
+        if($('#editPlaylist').length != 0 && $('#editPlaylist').val() != oldname) {
+          app.emit('UIEditPlaylist', $(that).data('id'));
+        }
+        $(that).html($('#editPlaylist').val());
+      };
+      
+      $('#editPlaylist').blur(editOne);
+      $('#editPlaylist').keypress(function(e) {
+        if(e.charCode == 13) {
+          editOne();
+        }
+      });
+      
+      $('#editPlaylist').dblclick(function(e) {
+        e.stopPropagation();
+      });
+    }
+  });
+  
+  //Sort
+  $('#treeview #list li').live('mousedown', function(e) {
+    var that = this;
+    var orig = e;
+    var itemDrag = function(e) {
+      if(Math.abs(orig.pageY - e.pageY) > 15) {
+        var centerY = e.pageY - ($(that).height() / 2);
+        $(that).css({ 
+          'width': $(that).width(),
+          'position': 'absolute',
+          'top': centerY,
+          'z-index': 1000
+        });
+      }
+    };
+    
+    var itemRelease = function(e) {
+      $(that).css({ 
+        'position': '',
+        'width': '',
+        'top': '',
+        'z-index': ''
+      });
+      $(document).unbind('mousemove', itemDrag);
+      $(document).unbind('mouseup', itemRelease);
+    };
+    
+    $(document).bind('mousemove', itemDrag);
+    $(document).bind('mouseup', itemRelease);
+  });  
+    
+  //Activate playlist
+  app.ui.activatePlaylist = function(id) {
+    var elem = $('li[data-id="'+id+'"]');
+    $('#treeview li').removeClass('selected');
+    elem.removeClass('clicked');
+    elem.addClass('selected');
+  }
+};
+
+/**
+ * Bind tracklist
+ * @param {Q.App} QPlayer
+ */
+Q.UITracklist = function(app) {
+  //Sort
+  $('#tracklist tr').live('mousedown', function(e) {
+    var orig = e;
+    var that = $(this);
+    var hasCloned = false;
+    var playListid = null;
+    
+    //Is it on a playlist
+    var playlistOver = function(e) {
+      console.log(e);
+    };
+    
+    var playlistOut = function(e) {
+      console.log(e);
+    };
+    
+    //Drag along
+    var itemDrag = function(e) {
+      if(Math.abs(orig.pageX - e.pageX) > 20 || Math.abs(orig.pageY - e.pageY) > 20) {
+        var centerY = e.pageY - (that.height() / 2);
+        var centerX = e.pageX - (that.height() / 2);
+        that.css({
+          'top': centerY,
+          'left': centerX
+        });
+      }
+    };
+    
+    //Delete on release
+    var itemRelease = function(e) {
+      if(hasCloned) {
+        that.remove();
+      }
+      $(document).unbind('mousemove', itemClone);
+      $(document).unbind('mousemove', itemDrag);
+      $(document).unbind('mouseup', itemRelease);
+      $('#treeview li').unbind('mouseover', playlistOver);
+      $('#treeview li').unbind('mouseout', playlistOut);
+    };
+    
+    //Make a duplicate
+    var itemClone = function(e) {
+      if(Math.abs(orig.pageX - e.pageX) > 20 || Math.abs(orig.pageY - e.pageY) > 20) {
+        var centerY = e.pageY - (that.height() / 2);
+        var centerX = e.pageX - (that.height() / 2);
+        that = that.clone();
+        that.find('td:first').prepend('<span class="plus"></span>');
+        hasCloned = true;
+        $('#tracklist').append(that);
+        that.css({
+          'position': 'absolute',
+          'z-index': 1000,
+          'top': centerY,
+          'left': centerX
+        });
+        
+        $('#treeview li').bind('mouseover', playlistOver);
+        $('#treeview li').bind('mouseout', playlistOut);
+        $(document).unbind('mousemove', itemClone);
+      }
+    };
+    
+
+    
+    $(document).bind('mousemove', itemClone);
+    $(document).bind('mousemove', itemDrag);
+    $(document).bind('mouseup', itemRelease);
+  });
+};
+
+/**
+ * Bind playback
+ * @param {Q.App} QPlayer
+ */
+Q.UIPlayback = function(app) {
   //Play-Pause button
   $('#playBtn').click(function() {
     $(this).toggleClass('pause');
@@ -171,7 +351,7 @@ Q.UIGeneric = function(app) {
     app.emit('UIPlayback', 'next');
   });
   
-  //Shuffle - Repeat buttons
+  //Shuffle
   $('#shuffle').click(function() {
     $(this).toggleClass('active');
     if($(this).hasClass('active')) {
@@ -181,6 +361,7 @@ Q.UIGeneric = function(app) {
     }
   });
   
+  //Repeat
   $('#repeat').click(function() {
     $(this).toggleClass('active');
     if($(this).hasClass('active')) {
@@ -188,5 +369,22 @@ Q.UIGeneric = function(app) {
     } else {
       app.emit('UIPlayback', 'repeatOff');
     }
+  });
+};
+
+/**
+ * Bind generic small objects
+ * @param {Q.App} QPlayer
+ */
+Q.UIGeneric = function(app) {
+
+  //Popup window
+  $('#miniBtn').click(function() {
+    var width = 250;
+    var height = 500;
+    app.popup = window.open('popup.html', 
+      'Player', 'menubar=no,location=no,resizable=no,scrollbars=no,status=no,'
+      + 'width=' + width + ',height=' + height + ',left=' + (window.outerWidth - (width+33))
+      + ',top=' + (window.screen.height - window.screen.availHeight+55));
   });
 };
